fac20fc35abb7be7b634e4af401a9b211fb7003a
thepipboy <3476647793@qq.com>
2025-03-16 19:20:07 +0800

    Update sum.py

--- sum.py
+++ sum.py
@@ -4,11 +4,12 @@ import RHS
 import sigma
 import multiply
 import equation
+import n
 class H(HS, HW):
     def __init__(self):
-        self.LHS = LHS()
-        self.midHS = midHS()
-        self.RHS = RHS()
+        self.LHS = LHS(n)
+        self.midHS = midHS(n)
+        self.RHS = RHS(n)
         self.equation = self.midHS  # Assuming midHS is an instance of a class
         self.sigma = self.LHS        # Assuming LHS is an instance of a class
         self.multiply = self.RHS      # Assuming RHS is an instance of a class
